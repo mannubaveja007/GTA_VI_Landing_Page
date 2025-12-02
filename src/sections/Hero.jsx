01f20880ffb88586986536ad64b58d5e020452bf
mannubaveja007 <23bis70088@cuchd.in>
2025-12-02 22:48:41 +0530

    Add: Hero Page Final Animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,46 @@
-import ComingSoon from './ComingSoon'
-
+import ComingSoon from './ComingSoon';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import  { useMaskSettings} from '../constants';
 const Hero = () => {
-  return (
+  const  {initialMaskPos, initialMaskSize, maskPos, maskSize} = useMaskSettings();
 
+  useGSAP(()=>{
+    gsap.set('.mask-wrapper',{
+      maskPosition : initialMaskPos,
+      maskSize : initialMaskSize,
+    })
+
+    gsap.set('.mask-logo' , {
+      marginTop : '-100vh',
+      opacity : 0,
+    })
+
+    gsap.set('.entrance-message' , {
+       marginTop : '0vh',
+    })
+
+    const tl = gsap.timeline({
+      scrollTrigger : {
+        trigger : '.hero-section', // target element
+        start : 'top top', // when the top of the trigger hits the top of the viewport
+        scrub :  2.5, // lag effect sync the animation to scroll position
+        end : '+=200%', // end after scrolling 200% of the viewport height
+        pin : true, // pin the section during the animation
+        // pin is useful for creating scroll based scenes and based text
+      }
+    });
+
+    tl
+      .to('.fade-out' , { opacity : 0 , ease : 'power1.inOut' })
+      .to('.scale-out' , { scale : 1 , ease : 'power1.inOut'})
+      .to('.mask-wrapper' , {maskSize : maskSize , ease : 'power1.inOut' } , "<")
+      .to('.mask-wrapper' , { opacity : 0 })
+      .to(".overlay-logo" , { opacity : 1 , onComplete : ()=>{gsap.to('.overlay-logo',{opacity :0})}} , "<")
+      .to('.entrance-message' , { ease : 'power1.inOut' , duration : 1 , maskImage : 'radial-gradient(circle at 50% 0vh , black  50% , transparent 100%)' } , "<")
+
+  })
+  return (
     <section className="hero-section">
       <div className="size-full mask-wrapper">
         <img src="/images/hero-bg.webp" alt="background" className="scale-out"/>

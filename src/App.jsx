d2ea44f417dfd3e19e58455214c4e56a4c0186a8
mannubaveja007 <23bis70088@cuchd.in>
2025-12-03 09:15:32 +0530

    Add: FirstVideo Animation Added + changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 import Hero from './sections/Hero'
 import Navbar from './sections/Navbar';
+import FirstVideo from './sections/FirstVideo';
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <main>
       <Navbar></Navbar>
       <Hero></Hero>
+      <FirstVideo />
     </main>
   )
 }
---
+++ src/sections/FirstVideo.jsx
@@ -0,0 +1,47 @@
+import { useRef } from "react"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const FirstVideo = () => {
+    const videoRef  = useRef(null);
+    useGSAP(()=>{
+            gsap.set(".first-vd-wrapper" , {
+                marginTop : "-150vh",
+                opacity : 0,
+            });
+
+            const tl = gsap.timeline({
+                scrollTrigger : {
+                    trigger : ".first-vd-wrapper",
+                    start : "top top",
+                    end : "+=200% top",
+                    scrub : true,
+                    pin : true,
+                }
+            })
+
+            tl.to(".hero-section", {delay : 0.5 , opacity : 0 , ease : 'power1.inOut'});
+            tl.to(".first-vd-wrapper" , {opacity : 1 , duration : 2 ,ease : 'power1.inOut'});
+            videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current , {currentTime : videoRef.current.duration , ease : 'power1.inOut' , duration : 3},"<");
+            }
+
+    },[])
+
+    return (
+    <section className="first-vd-wrapper">
+            <div className="h-dvh">
+                <video src="/videos/output1.mp4" 
+                ref={videoRef} 
+                playsInline 
+                muted 
+                preload="auto" 
+                className="first-vd"
+                ></video>
+
+            </div>
+    </section>
+  )
+}
+
+export default FirstVideo
